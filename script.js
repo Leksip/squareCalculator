cd9267a21732f7c5a3660a8f16036806d92f0bcc
Nikita <106311919+Leksip@users.noreply.github.com>
2022-09-12 19:25:39 +0300

    Update script.js
    1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const button = document.querySelector('.btn'),
 let calculation = 0;
 
 button.addEventListener('click', () => {
-  calculation = (+long.value * +width.value).toFixed(1);
+  calculation = (long.value * width.value).toFixed(1);
   result.innerHTML = `<h1 class="result" > ${calculation} см²</h1>
 <h3 class="result">${calculation / 100} м²</h3>`;
   width.value = long.value = '';
