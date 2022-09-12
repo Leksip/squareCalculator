51e2743e949e653b4ed563a3bdf98ab666e49f7e
Leksip <Nickita.rom@gmail.com>
2022-09-12 18:49:49 +0300

    Создание простого калькулятора площади

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.btn'),
+    long = document.querySelector('#long'),
+    width = document.querySelector('#width'),
+    result = document.querySelector('#result');
+
+
+let calculation = 0;
+
+button.addEventListener('click', () => {
+  calculation = (long.value * width.value).toFixed(1);
+  result.innerHTML = `<h1 class="result" > ${calculation} см</h1>
+<h3 class="result">${calculation / 100} м</h3>`;
+  width.value = long.value = '';
+});
